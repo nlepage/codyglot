abdeda031d173831dfb5bcedaab4887dc2e56382
nlepage <19571875+nlepage@users.noreply.github.com>
2018-11-13 16:24:37 +0100

    :fire: Remove REPL

--- repl/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Write nginx config
-envsubst '\${GATEWAY_ENDPOINT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-# Start nginx
-exec nginx -g 'daemon off;'
